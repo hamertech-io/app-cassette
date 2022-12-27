2b5ee7af97c8c72aa11a258acfc8c5e03614ccbc
hamertech-io <121461847+hamertech-io@users.noreply.github.com>
2022-12-27 11:45:12 -0800

    delete unused import

--- api/index.js
+++ api/index.js
@@ -1,5 +1,3 @@
-import { BASE_API_URL } from "../constants";
-
 export const fetchCassette = async () => {
   return fetch(`https://cassette-json.s3.us-west-1.amazonaws.com/data.json`, {
     method: "GET",
